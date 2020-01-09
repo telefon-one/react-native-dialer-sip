9f6d98f196a78004196b2742b5b5d3c9e4155cc1
cpodimas@gmail.com <cpodimas@gmail.com>
2020-01-09 11:11:30 +0300

    auto

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 ./git_update.sh
-yarn add https://github.com/telefon-one/react-native-tele https://github.com/telefon-one/react-native-replace-dialer https://github.com/telefon-one/react-native-telefon-sip
+#yarn add https://github.com/telefon-one/react-native-tele https://github.com/telefon-one/react-native-replace-dialer https://github.com/telefon-one/react-native-telefon-sip
 yarn android
